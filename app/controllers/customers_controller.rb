1af6f481f5c56c0965ce12ff02a63c55ad836e4c
MarioOimet <mario.oimet@gmail.com>
2017-11-09 15:20:33 -0600

    Part 2 finished, views to alphabetized, missing emails and index view plus CSS

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,12 @@ class CustomersController < ApplicationController
   def index
     @customer = Customer.all
   end
+
+  def alphabetized
+    @customer = Customer.order(:full_name)
+  end
+
+  def missing_email
+    @customer = Customer.where('email_address = ""')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
+  get '/customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized'
+  get '/customers/missing_email', to: 'customers#missing_email', as: 'missing_email'
   root to: 'customers#index', as: 'home'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
